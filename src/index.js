ab77cb638b0563738f48d0ed3545eee9aace9359
Sergii Marchenko <marikmarchelo@gmail.com>
2024-08-04 13:24:19 +0300

    add commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from './components/App';
 import './index.css';
+import data from './data.json';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const divElCreate = React.createElement('div', { data });
+const root = document.getElementById('root');
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <data />
+//   </React.StrictMode>
+// );
+
+const userData = {
+  username: 'Jacques Gluke',
+  tag: 'jgluke',
+  location: 'Ocho Rios, Jamaica',
+  avatar: 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+
+ReactDOM.createRoot(root).render(userData);
+console.log(divElCreate);
+
+// const App = () => {
+//   return (
+//     <>
+//            {data}
+//       <Profile
+//         name={data.username}
+//         tag={data.tag}
+//         location={data.location}
+//         image={data.avatar}
+//         stats={data.stats}
+//       />
+//          {' '}
+//     </>
+//   );
+// };
