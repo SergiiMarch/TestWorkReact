1a39d948cb15a6d1391d8a56cfcfa76e1494acc0
Sergii Marchenko <marikmarchelo@gmail.com>
2024-08-04 14:12:42 +0300

    add card js

--- src/index.js
+++ src/index.js
@@ -7,26 +7,46 @@ import data from './data.json';
 const divElCreate = React.createElement('div', { data });
 const root = document.getElementById('root');
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <data />
-//   </React.StrictMode>
-// );
-
-const userData = {
-  username: 'Jacques Gluke',
-  tag: 'jgluke',
-  location: 'Ocho Rios, Jamaica',
-  avatar: 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308,
-  },
-};
-
-ReactDOM.createRoot(root).render(userData);
+const userCard = (
+  <div>
+    <div>
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+        alt="User avatar"
+      />
+      <p>Petra Marica</p>
+      <p>@pmarica</p>
+      <p>Salvador, Brasil</p>
+    </div>
+    <ul>
+      <li>
+        <span>Followers</span>
+        <span>1000</span>
+      </li>
+      <li>
+        <span>Views</span>
+        <span>2000</span>
+      </li>
+      <li>
+        <span>Likes</span>
+        <span>3000</span>
+      </li>
+    </ul>
+  </div>
+);
+ReactDOM.createRoot(root).render(userCard);
 console.log(divElCreate);
+// const userData = {
+//   username: 'Jacques Gluke',
+//   tag: 'jgluke',
+//   location: 'Ocho Rios, Jamaica',
+//   avatar: 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
+//   stats: {
+//     followers: 5603,
+//     views: 4827,
+//     likes: 1308,
+//   },
+// };
 
 // const App = () => {
 //   return (
