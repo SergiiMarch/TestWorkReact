655effddfb5eef23b2f347baa80330afd35f1726
Sergii Marchenko <marikmarchelo@gmail.com>
2024-08-04 15:59:52 +0300

    add commit

--- src/index.js
+++ src/index.js
@@ -4,38 +4,34 @@ import { App } from './components/App';
 import './index.css';
 import data from './data.json';
 
-const divElCreate = React.createElement('div', { data });
 const root = document.getElementById('root');
 
 const userCard = (
   <div>
     <div>
-      <img
-        src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-        alt="User avatar"
-      />
-      <p>Petra Marica</p>
-      <p>@pmarica</p>
-      <p>Salvador, Brasil</p>
+      <img src={data.avatar} alt="User avatar" />
+      <p>{data.username}</p>
+      <p>{data.tag}</p>
+      <p>{data.location}</p>
     </div>
     <ul>
       <li>
         <span>Followers</span>
-        <span>1000</span>
+        <span>{data.stats.followers}</span>
       </li>
       <li>
         <span>Views</span>
-        <span>2000</span>
+        <span>{data.stats.views}</span>
       </li>
       <li>
         <span>Likes</span>
-        <span>3000</span>
+        <span>{data.stats.likes}</span>
       </li>
     </ul>
   </div>
 );
 ReactDOM.createRoot(root).render(userCard);
-console.log(divElCreate);
+console.log(userCard);
 // const userData = {
 //   username: 'Jacques Gluke',
 //   tag: 'jgluke',
@@ -63,3 +59,4 @@ console.log(divElCreate);
 //     </>
 //   );
 // };
+console.log(data);
